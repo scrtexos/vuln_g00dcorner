3ced41d391c5ff8922a60c6db2b2ee86413a3a9a
agix <florian.gaultier@gmail.com>
2014-08-28 13:01:29 +0200

    Little bug in js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    if($("#Category").val()!='new'){
+        $("#new_category").hide();
+    }
+    else{
+        $("#new_category").show();
+    }
+
    $("#Category").change(function(){
     if($("#Category").val()!='new'){
         $("#new_category").hide();
